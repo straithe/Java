8ae930e06848d49a7aa99f6ee86c28c60f9a4163
Straithe <iamstraithe@gmail.com>
2015-03-16 13:50:39 -0500

    Adding a Hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//Hello World program
+//2015 - 03 - 16
+
+import java.util.*;
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World.");
+  }
+}
