49b538cc08ac4a11e5fb35cfb4aa1d85319591a6
Straithe <iamstraithe@gmail.com>
2015-03-25 15:10:54 -0500

    Added a new method and adjusted comments.

--- StateMachine/StateMachine.java
+++ StateMachine/StateMachine.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Small state machine that prints 1 only after seeing the pattern 110, 
  *   otherwise a 0 is printed. 
  * 2015 - 03 - 25
@@ -11,28 +11,29 @@ public class StateMachine
 {
   public static void main(String[] args)
   {
-    //Variable declarations
+    //Variable declarations.
     String input;
     int inputBit;
     int state = 0;
     Scanner scanner = new Scanner(System.in);
 
-    //Takes in one bit at a time from user to adjust the
+    //Takes in one bit at a time from the user to adjust the
     //  current state.
     do
     {
-      input = scanner.nextLine(); //Reads input from user            
-      inputBit = Integer.parseInt(input); //Converts string to integer
+      input = scanner.nextLine(); //Reads input from user.
+      inputBit = Integer.parseInt(input); //Converts string to integer.
 
       if(inputBit == 1 || inputBit == 0)
       {
         state = nextState(inputBit, state);
+        printInformation(inputBit, state);
       }
     }while(inputBit!= 2);
   }
 
   /*
-   * nextState
+   * nextState(int inputBit, int state)
    * 
    * Purpose: Adjusts the state based on the current state, and the input
    *            bit received.
@@ -89,8 +90,22 @@ public class StateMachine
         break;
     }
 
+    return state;
+  }
+
+  /*
+   * printInformation(int inputBit, int state)
+   *
+   * PURPOSE:  Prints out input as described by the program purpose.
+   *
+   * @param    int inputBit    The input bit received from the user.
+   * @param    int state       The current state of the state machine.
+   */
+  public static void printInformation(int inputBit, int state)
+  {
     //Output tree that uses the state to print out either a 1 or 0 as needed.
     System.out.print("State: " + state + " bit: " + inputBit + " Output:");
+
     if (state == 3)
     {
       System.out.print(" 1\n");        } 
@@ -98,7 +113,5 @@ public class StateMachine
     {
       System.out.print(" 0\n");
     } 
-
-    return state;
   }
 }
